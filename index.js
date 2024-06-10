f037be957d0894fb09469ccc894ee370390d51eb
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-10 22:55:27 +0200

    linked the script file

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let billAmountElement = document.querySelector("#bill-amount");
 let percentageAmountElement = document.querySelector("#tip-percentage");
 let totalAmountOutput = document.querySelector("#total-amount");
 
-function calculateTotal() {}
+function calculateTotal(event) {
+  event.preventDefault();
+  percentageAmountElement = percentageAmountElement.value / 100;
+  totalAmountOutput = billAmountElement.value + percentageAmountElement;
+}
 
 let calculateButton = document.querySelector("#calculate-btn");
 calculateButton.addEventListener("click", calculateTotal);
