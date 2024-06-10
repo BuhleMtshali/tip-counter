9801048684c722dec44442c30f65ebb2c0532b49
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-10 22:46:36 +0200

    updated the javascript file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
- 
+let billAmountElement = document.querySelector("#bill-amount");
+let percentageAmountElement = document.querySelector("#tip-percentage");
+let totalAmountOutput = document.querySelector("#total-amount");
+
+function calculateTotal() {}
+
+let calculateButton = document.querySelector("#calculate-btn");
+calculateButton.addEventListener("click", calculateTotal);
