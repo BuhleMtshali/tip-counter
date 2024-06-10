dc2fc061f99d73371a5a33742a68eb847911e6c9
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-10 23:07:00 +0200

    updated the index file

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ let totalAmountOutput = document.querySelector("#total-amount");
 
 function calculateTotal(event) {
   event.preventDefault();
-  percentageAmountElement = percentageAmountElement.value / 100;
-  totalAmountOutput = billAmountElement.value + percentageAmountElement;
+  let billAmount = parseFloat(billAmountElement.value); //Parsing Values: The billAmount and tipPercentage values are parsed as floats using parseFloat to ensure numerical calculations.
+  let tipPercentage = parseFloat(percentageAmountElement.value) / 100;
+  let totalAmount = billAmount + billAmount * tipPercentage;
+  totalAmountOutput.textContent = totalAmount.toFixed(2);
 }
 
 let calculateButton = document.querySelector("#calculate-btn");
